7f517b7636908f27cb4da02346442f2331293688
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-09-28 17:18:46 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+#Faça um Programa que converta metros para centímetros.
+num = float(input("Escreva um número em metro para converter em centímetros:"))
+soma = str(num * 100)
+print("Conversão de metros para centímetros ficou assim: " + soma)
